3133f5e31c3e26d9ce464b162a01e4ebe0b02dea
gs256 <regularnetuser@gmail.com>
2023-01-06 18:48:24 +0300

    Implement batch video composing

--- ffmpeg_command.py
+++ ffmpeg_command.py
@@ -1,25 +1,40 @@
-import glob
 import config
+import subprocess
 
-def generate_command() -> str:
+transition_duration = 1
+
+def generate_for_images(number_images: list, competitor_images: list, out_file: str) -> str:
     number_duration = 2
     competitor_duration = 3
-    transition_duration = 1
-    template = "ffmpeg {input} -c:v libx264 -filter_complex_script {filter} -pix_fmt yuvj422p -framerate 20 out/out.mp4"
-    numbers_wildcard = "out/numbers/*.*"
-    competitors_wildcard = "out/competitors/*.*"
-    number_images = sorted(glob.glob(numbers_wildcard), reverse=True)
-    competitor_images = sorted(glob.glob(competitors_wildcard), reverse=True)
+    template = "ffmpeg {input} -c:v libx264 -filter_complex_script {filter} -pix_fmt yuvj422p -framerate 20 -preset ultrafast {out_file}"
     assert(len(number_images) > 0)
     assert(len(competitor_images) > 0)
     input_count = len(number_images) * 2
-    input = generate_ffmpeg_input(number_images, competitor_images, number_duration, competitor_duration, transition_duration)
-    filter = generate_complex_filter(input_count, config.WIDTH, config.HEIGHT, transition_duration, number_duration, competitor_duration)
-    command = template.format(input=input, filter=filter)
+    input = generate_image_input(number_images, competitor_images, number_duration, competitor_duration, transition_duration)
+    filter = generate_filter_for_images(input_count, config.WIDTH, config.HEIGHT, transition_duration, number_duration, competitor_duration)
+    command = template.format(input=input, filter=filter, out_file=out_file)
     return command
 
 
-def generate_ffmpeg_input(number_images: list, competitor_images: list, number_duration: int, competitor_duration: int, transition_duration: int) -> str:
+def generate_for_videos(videos: list, out_file: str) -> str:
+    template = "ffmpeg {input} -filter_complex \"{filter}\" -preset ultrafast {out_file}"
+    assert(len(videos) > 0)
+    input = generate_video_input(videos)
+    filter = generate_filter_for_videos(videos, transition_duration)
+    command = template.format(input=input, filter=filter, out_file=out_file)
+    return command
+
+
+def generate_video_input(videos: list) -> str:
+    result = ""
+
+    for video in videos:
+        result += f" -i {video} "
+
+    return result
+
+
+def generate_image_input(number_images: list, competitor_images: list, number_duration: int, competitor_duration: int, transition_duration: int) -> str:
     assert(len(number_images) <= len(competitor_images))
     result = ""
 
@@ -31,7 +46,39 @@ def generate_ffmpeg_input(number_images: list, competitor_images: list, number_d
     return result
 
 
-def generate_complex_filter(input_count: int, width: int, height: int, transition_duration: int, number_duration: int, competitor_duration: int) -> str:
+def generate_filter_for_videos(videos: list, transition_duration: int) -> str:
+    template = "[{input1}][{input2}]xfade=radial:duration={duration}:offset={offset}"
+    result = ""
+
+    def calculate_offset(videos: list, index: int, transition_duration: int) -> int:
+        result = 0
+        for i in range(index+1):
+            result += get_video_duration(videos[i])
+        result -= transition_duration
+        return result
+
+    for i in range(len(videos)-1):
+        offset = calculate_offset(videos, i, transition_duration)
+        input1 = f"{i}:v" if i == 0 else f"v{i}o"
+        addition = template.format(input1=input1, input2=f"{i+1}:v", duration=transition_duration, offset=offset)
+        if i < len(videos) - 2:
+            addition += "[v{next}o];".format(next=i+1)
+        result += addition
+
+    return result
+
+
+def get_video_duration(path: str) -> int:
+    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
+                             "format=duration", "-of",
+                             "default=noprint_wrappers=1:nokey=1", path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+
+    return int(float(result.stdout.decode()))
+
+
+def generate_filter_for_images(input_count: int, width: int, height: int, transition_duration: int, number_duration: int, competitor_duration: int) -> str:
     input_config_template = "[{index}:v]scale={width}:{height}:force_original_aspect_ratio=decrease,pad={width}:{height}:(ow-iw)/2:(oh-ih)/2,setsar=1[v{index}];"
     transition_template = "[{input1}][{input2}]xfade=transition=radial:duration={duration}:offset={offset}"
     transition_output_template = "[v{next}o];"
@@ -56,5 +103,5 @@ def generate_complex_filter(input_count: int, width: int, height: int, transitio
     return out_path
 
 
-result = generate_command()
-print(result)
+# result = generate()
+# print(result)
---
+++ main.py
@@ -0,0 +1,45 @@
+import ffmpeg_command
+import glob
+import subprocess
+import os
+
+# FIXME: automate all the previous steps
+
+numbers_wildcard = "out/numbers/*.*"
+competitors_wildcard = "out/competitors/*.*"
+number_images = sorted(glob.glob(numbers_wildcard), reverse=True)
+competitor_images = sorted(glob.glob(competitors_wildcard), reverse=True)
+
+assert(len(number_images) <= len(competitor_images))
+top_length = len(number_images)
+
+if len(competitor_images) > top_length:
+    competitor_images = competitor_images[:top_length]
+
+# number of simultaneously opened files will be twice as big
+max_batch_size = 200
+pictures_done = 0
+
+
+def make_batch(offset: int, count: int):
+    command = ffmpeg_command.generate_for_images(number_images[offset:offset+count], competitor_images[offset:offset+count], f"out/batches/out_{offset:010d}.mp4")
+    result = subprocess.run(command, shell=True)
+    if result.returncode != 0:
+        print(f"Error occured. Exiting")
+        exit()
+
+
+if top_length <= max_batch_size:
+    command = ffmpeg_command.generate_for_images(number_images, competitor_images, f"out/top.mp4")
+    subprocess.run(command, shell=True)
+else:
+    os.makedirs("out/batches", exist_ok=True)
+
+    while pictures_done < top_length:
+        make_batch(pictures_done, max_batch_size)
+        pictures_done += max_batch_size
+
+    batches = sorted(glob.glob("out/batches/*"))
+    print(f"Found {len(batches)} batches")
+    merge_command = ffmpeg_command.generate_for_videos(batches, "out/top.mp4")
+    subprocess.run(merge_command, shell=True)
